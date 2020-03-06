b0a40a85d3fa2d2f313ede112def3ff30f0623ca
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2020-03-06 17:39:32 +0100

    Implement Show more

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,49 @@
+/* ========== SHOW MORE ========== */
+
+const bioContent = document.getElementById("bioContent");
+const bioFirstParagraph = document.querySelector(
+    ".biography__content p:first-of-type"
+);
+const showMoreLink = document.getElementById("showMoreLink");
+const showMoreLinkIcon = document.getElementById("showMoreLinkIcon");
+const showMoreLinkText = document.getElementById("showMoreLinkText");
+let showingMore = false;
+
+showLess();
+
+function showMore() {
+    bioContent.style.maxHeight = bioContent.scrollHeight + "px";
+    showMoreLinkIcon.innerText = "keyboard_arrow_up";
+    showMoreLinkText.innerText = "Show less";
+    showingMore = true;
+}
+
+function showLess() {
+    bioContent.style.maxHeight = bioFirstParagraph.scrollHeight + "px";
+    showMoreLinkIcon.innerText = "keyboard_arrow_down";
+    showMoreLinkText.innerText = "Show more";
+    showingMore = false;
+}
+
+showMoreLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    if (showingMore) {
+        showLess();
+    } else {
+        showMore();
+    }
+});
+
+window.addEventListener("resize", function() {
+    if (showingMore) {
+        showMore();
+    } else {
+        showLess();
+    }
+});
+
+/* ========== BUTTON HOVER EFFECT ========== */
+
 const buttons = document.querySelectorAll(".button");
 
 buttons.forEach(button => {
